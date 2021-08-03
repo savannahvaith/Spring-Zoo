85432f39d0d88b193a6cf74acde32fd655ce4124
savannahvaith <sav.vaithilingam@gmail.com>
2021-08-03 11:13:24 +0100

    SV: Initial Structure

---
+++ src/main/java/com/qa/springzoo/SpringZooApplication.java
@@ -0,0 +1,13 @@
+package com.qa.springzoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringZooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringZooApplication.class, args);
+	}
+
+}
